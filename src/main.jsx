458272528c3f060392909d6203501ea936f93f0c
jakubamlenovsky <eaa24jmal@students.eaaa.dk>
2026-05-21 14:35:42 +0200

    chore: configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { UserProvider } from './context/UserContext'
 import './styles.css'
@@ -8,9 +8,9 @@ import './styles.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </UserProvider>
   </React.StrictMode>,
 )
